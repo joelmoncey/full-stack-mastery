9ab88c624f882b4f2631b55e04d35700712d2128
Your Name <joelmoncey10@gmail.com>
2026-03-13 20:59:37 +0530

    jsstart

---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+let name = "joel moncey";
+console.log(typeof name);
+let age = 45
+console.log(typeof age);
+let number = 4324769623743748749328758735987598237598326593265932659387459832759832759873765765876587585875786875879687686857575757n
+console.log(typeof number);
+let married = false;
+console.log(typeof married);
+let symbol = Symbol("&");
+console.log(typeof symbol);
+alert("joel moncey");
+let daddy;
+console.log(daddy)
