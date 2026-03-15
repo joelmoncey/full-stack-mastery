90c6488ade9e2722debbac8185a79b8366e4d222
Your Name <joelmoncey10@gmail.com>
2026-03-15 20:50:47 +0530

    non-primitive

---
+++ Javascript/non-primitive.js
@@ -0,0 +1,18 @@
+let object = {
+    place:"karunagapally",
+    pincode: 376723
+}
+console.log(object)
+
+let array = [
+    "apple","orange","grape"
+]
+console.log(array)
+function sum()
+{
+   let a =2;
+   let b =3; 
+   let sum=a+b
+   console.log(sum)
+}
+sum()
