c46c786e5fd74189043d73f8e3611269b5c7729f
Your Name <joelmoncey10@gmail.com>
2026-03-17 20:51:17 +0530

    operators

---
+++ Javascript/operators.js
@@ -0,0 +1,22 @@
+//arithemetic operater
+let x=10
+let y=5
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+// assignment operators
+let j=10
+console.log(j+=5)
+console.log(j-=5)
+console.log(j/=5)
+console.log(j*=5)
+// comparison operators
+console.log(j<x)
+// logical operators
+console.log(j==x || y>x)
+//Bitwise operators
+console.log(4|2)
+//Ternary operators
+let b= x==j ? "hi":"hello"
+console.log(b)
