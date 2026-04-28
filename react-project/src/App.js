87afb74c49c201e157d137d5d0ca2a54c9f0b26b
Your Name <joelmoncey10@gmail.com>
2026-04-28 20:53:23 +0530

    react-resume

---
+++ react-project/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          JOEL MONCEY
+        </p>
+        <a
+          className="App-link"
+          href="https://joelmoncey.github.io/Resume-/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          RESUME
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
